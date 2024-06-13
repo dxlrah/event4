55211230b5642f6e0f9f14228f310b5831ec53a9
anderson.delrah@live.com <anderson.delrah@live.com>
2024-06-13 17:51:02 +0200

    sixth commit.

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
   publicRoutes: [
-    '/',
+    '/', // Public routes accessible without authentication
     '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
     '/api/uploadthing'
   ],
   ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
+    '/api/webhook/clerk', // Ignored routes not processed by middleware
+    '/api/webhook/stripe'
   ]
 });
- 
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
